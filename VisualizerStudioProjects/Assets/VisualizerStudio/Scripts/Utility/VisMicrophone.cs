5d366f12f0c461d9c34ac8e7fc435f9de054630a
Ante <ante@xmgstudio.com>
2013-12-03 16:58:13 -0500

    Adding VisualizerStudio package files.
    - Ignoring .DS_Store files.
    
    Signed-off-by: Ante <ante@xmgstudio.com>

---
+++ VisualizerStudioProjects/Assets/VisualizerStudio/Scripts/Utility/VisMicrophone.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This class should be attached to the game object that contains an audio source.  If enabled, it will replace 
+/// the current clip on the audio source with the microphone input.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+[AddComponentMenu("Visualizer Studio/Utility/Microphone")]
+public class VisMicrophone : MonoBehaviour
+{
+    public string microphoneDevice = "Default";
+
+    void Start()
+    {
+        //make sure it's okay to start the microphone
+        if (audio && Microphone.devices != null && Microphone.devices.Length > 0)
+        {
+            //get index
+            int index = 0;
+            for (int i = 0; i < Microphone.devices.Length; i++)
+            {
+                if (Microphone.devices[i].Equals(microphoneDevice, System.StringComparison.CurrentCultureIgnoreCase))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            //assign and start microphone and then mute the audio source
+            audio.clip = Microphone.Start(Microphone.devices[index], true, 999, AudioSettings.outputSampleRate);
+            audio.mute = true;
+
+            //HACK - Wait for microphone to be ready
+            while (!(Microphone.GetPosition(Microphone.devices[index]) > 0)) { }
+
+            //start the audio source
+            audio.Play();
+        }
+        else if (audio)
+            Debug.LogWarning("No audio source was found, can't start microphone! You must attach thise script to the same Game Object as an Audio Source.");
+        else if (Microphone.devices == null || Microphone.devices.Length <= 0)
+            Debug.LogWarning("No microphone devices were found, can't start microphone!");
+        else
+            Debug.LogWarning("Unknown issue, can't start microphone!");
+    }
+
+}
